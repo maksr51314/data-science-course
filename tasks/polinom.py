f1d576e93c5bef6b0ef628336bcc95054edd64b8
Maxim Romaniv <Maxim.Romaniv@netent.com>
2017-11-13 10:01:10 +0200

    polinom

---
+++ tasks/polinom.py
@@ -0,0 +1,15 @@
+def polinom(coefficients):
+    def polinom_wrapper(x):
+        sum = 0
+        for index, coef in enumerate(coefficients):
+            val = coef * x ** (len(coefficients) - 1 - index)
+            sum += val
+
+        return sum
+    return polinom_wrapper
+
+
+
+res = polinom([1, 2, 3])(2)
+
+print(res)
